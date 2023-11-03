323291f355429a3e71475b690e7bfc546f1a2e5b
njwnabilah <146785619+njwnabilah@users.noreply.github.com>
2023-11-03 16:02:37 +0700

    Add files via upload

---
+++ Task4.java
@@ -0,0 +1,10 @@
+public class Task4 {
+    public static void main(String[] args) {
+        String[] minuman = { "jus mangga", "jus jeruk", "jus melom", "jus jambu", "jus alpukat", "jus apel"};
+
+        System.out.println("jus buah yang tersedia");
+        for (int i = 0; i < minuman.length; i++) {
+            System.out.println(minuman [i]);
+        }
+    }
+}
